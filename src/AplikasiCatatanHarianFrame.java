01f90ebdf1ad6a188a8284c1ab524db77aaf30fc
MuhammadZailani24 <mzailani728@gmail.com>
2025-11-15 14:56:59 +0700

    Menambahkan Kode Pada Setiap Button

--- src/AplikasiCatatanHarianFrame.java
+++ src/AplikasiCatatanHarianFrame.java
@@ -8,29 +8,16 @@ import java.awt.event.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import javax.swing.table.DefaultTableModel;
+import java.io.*;
 
 /**
  *
  * @author Acer
  */
 public class AplikasiCatatanHarianFrame extends javax.swing.JFrame {
-
-  private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton5;
-    private javax.swing.JButton jButton6;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private DefaultTableModel tableModel; // Table model for storing notes
-
+    
+ private DefaultTableModel tableModel;
+ 
     public AplikasiCatatanHarianFrame() {
         initComponents();
         tableModel = new DefaultTableModel(new Object[]{"Judul Catatan", "Isi Catatan", "Tanggal/Waktu"}, 0);
@@ -72,6 +59,11 @@ public class AplikasiCatatanHarianFrame extends javax.swing.JFrame {
 
         jButton1.setForeground(new java.awt.Color(255, 0, 51));
         jButton1.setText("Keluar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setText("Hapus");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
@@ -90,6 +82,11 @@ public class AplikasiCatatanHarianFrame extends javax.swing.JFrame {
 
         jButton4.setForeground(new java.awt.Color(0, 204, 51));
         jButton4.setText("Tambah");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -206,20 +203,85 @@ public class AplikasiCatatanHarianFrame extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+         int selectedRow = jTable1.getSelectedRow();
+        if (selectedRow != -1) {
+            tableModel.removeRow(selectedRow);
+        }
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+        int selectedRow = jTable1.getSelectedRow();
+        if (selectedRow != -1) {
+            String judul = (String) tableModel.getValueAt(selectedRow, 0);
+            String isi = (String) tableModel.getValueAt(selectedRow, 1);
+
+            jTextField1.setText(judul);
+            jTextField2.setText(isi);
+
+            // Mengupdate baris dengan nilai baru
+            tableModel.setValueAt(jTextField1.getText(), selectedRow, 0);
+            tableModel.setValueAt(jTextField2.getText(), selectedRow, 1);
+        }
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
-        // TODO add your handling code here:
+          // Membuka JFileChooser untuk memilih lokasi dan nama file
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Save File As");
+        fileChooser.setSelectedFile(new File("catatan_harian.txt"));
+        
+        // Menyimpan file jika pengguna memilih lokasi dan klik "Save"
+        int userSelection = fileChooser.showSaveDialog(this);
+        if (userSelection == JFileChooser.APPROVE_OPTION) {
+            File fileToSave = fileChooser.getSelectedFile();
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileToSave))) {
+                for (int i = 0; i < tableModel.getRowCount(); i++) {
+                    String judul = (String) tableModel.getValueAt(i, 0);
+                    String isi = (String) tableModel.getValueAt(i, 1);
+                    String tanggal = (String) tableModel.getValueAt(i, 2);
+                    writer.write(judul + "|" + isi + "|" + tanggal); // Menyimpan data dengan pemisah "|"
+                    writer.newLine();
+                }
+                JOptionPane.showMessageDialog(this, "File TXT berhasil disimpan!");
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(this, "Gagal menyimpan file TXT!");
+            }
+        }
     }//GEN-LAST:event_jButton5ActionPerformed
 
     private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
-        // TODO add your handling code here:
+      JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Open TXT File");
+
+        int userSelection = fileChooser.showOpenDialog(this);
+        if (userSelection == JFileChooser.APPROVE_OPTION) {
+            File fileToOpen = fileChooser.getSelectedFile();
+            try (BufferedReader reader = new BufferedReader(new FileReader(fileToOpen))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] data = line.split("\\|");
+                    tableModel.addRow(data);
+                }
+                JOptionPane.showMessageDialog(this, "File berhasil dimuat!");
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(this, "Gagal memuat file TXT!");
+            }
+        }
     }//GEN-LAST:event_jButton6ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+         String judul = jTextField1.getText();
+        String isi = jTextField2.getText();
+        String tanggalWaktu = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss").format(new Date());
+
+        tableModel.addRow(new Object[]{judul, isi, tanggalWaktu});
+        jTextField1.setText("");
+        jTextField2.setText("");
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
